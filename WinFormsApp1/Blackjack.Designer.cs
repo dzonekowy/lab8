380d0e7d6ebdf36099087944b0e731b523c12910
adam09205 <adam.panczak50@gmail.com>
2026-05-11 22:29:19 +0200

    Dodano symulacje talii (tasowanie, dobieranie) Dodano wizualizacje ręki i sumowanie wartości Dodano okienko ze zdarzeniami Dodano przyciski dobierania i pasowania Dodano ukrywanie i odkrywanie 1 karty dealera Dodano mechanizm soft 17 dla dealera

--- WinFormsApp1/Blackjack.Designer.cs
+++ WinFormsApp1/Blackjack.Designer.cs
@@ -34,130 +34,186 @@
             zaklad_numeric = new NumericUpDown();
             saldo_label = new Label();
             saldo_ilosc = new Label();
-            dealer_karty = new DataGridView();
-            gracz_karty = new DataGridView();
             suma_dealer = new TextBox();
             suma_gracz = new TextBox();
             suma_label = new Label();
+            reka_dealer = new FlowLayoutPanel();
+            reka_gracz = new FlowLayoutPanel();
+            button_hit = new Button();
+            button_hold = new Button();
+            suma_label2 = new Label();
+            log_box = new RichTextBox();
             ((System.ComponentModel.ISupportInitialize)zaklad_numeric).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)dealer_karty).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)gracz_karty).BeginInit();
             SuspendLayout();
             // 
             // dealer_label
             // 
             dealer_label.AutoSize = true;
-            dealer_label.Location = new Point(94, 88);
+            dealer_label.Location = new Point(49, 173);
+            dealer_label.Margin = new Padding(2, 0, 2, 0);
             dealer_label.Name = "dealer_label";
-            dealer_label.Size = new Size(62, 25);
+            dealer_label.Size = new Size(40, 15);
             dealer_label.TabIndex = 0;
             dealer_label.Text = "Dealer";
+            dealer_label.Click += dealer_label_Click;
             // 
             // gracz_label
             // 
             gracz_label.AutoSize = true;
-            gracz_label.Location = new Point(94, 210);
+            gracz_label.Location = new Point(49, 558);
+            gracz_label.Margin = new Padding(2, 0, 2, 0);
             gracz_label.Name = "gracz_label";
-            gracz_label.Size = new Size(59, 25);
+            gracz_label.Size = new Size(38, 15);
             gracz_label.TabIndex = 1;
             gracz_label.Text = "label2";
             // 
             // start_button
             // 
-            start_button.Location = new Point(775, 491);
+            start_button.Location = new Point(752, 681);
+            start_button.Margin = new Padding(2);
             start_button.Name = "start_button";
-            start_button.Size = new Size(112, 34);
+            start_button.Size = new Size(78, 23);
             start_button.TabIndex = 2;
             start_button.Text = "Start";
             start_button.UseVisualStyleBackColor = true;
             // 
             // zaklad_numeric
             // 
-            zaklad_numeric.Location = new Point(745, 439);
+            zaklad_numeric.Location = new Point(727, 645);
+            zaklad_numeric.Margin = new Padding(2);
             zaklad_numeric.Name = "zaklad_numeric";
-            zaklad_numeric.Size = new Size(180, 31);
+            zaklad_numeric.Size = new Size(126, 23);
             zaklad_numeric.TabIndex = 4;
             // 
             // saldo_label
             // 
             saldo_label.AutoSize = true;
-            saldo_label.Location = new Point(788, 400);
+            saldo_label.Location = new Point(752, 615);
+            saldo_label.Margin = new Padding(2, 0, 2, 0);
             saldo_label.Name = "saldo_label";
-            saldo_label.Size = new Size(61, 25);
+            saldo_label.Size = new Size(39, 15);
             saldo_label.TabIndex = 5;
             saldo_label.Text = "Saldo:";
+            saldo_label.Click += saldo_label_Click;
             // 
             // saldo_ilosc
             // 
             saldo_ilosc.AutoSize = true;
-            saldo_ilosc.Location = new Point(842, 400);
+            saldo_ilosc.Location = new Point(795, 615);
+            saldo_ilosc.Margin = new Padding(2, 0, 2, 0);
             saldo_ilosc.Name = "saldo_ilosc";
-            saldo_ilosc.Size = new Size(32, 25);
+            saldo_ilosc.Size = new Size(19, 15);
             saldo_ilosc.TabIndex = 6;
             saldo_ilosc.Text = "20";
             // 
-            // dealer_karty
-            // 
-            dealer_karty.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dealer_karty.Location = new Point(189, 65);
-            dealer_karty.Name = "dealer_karty";
-            dealer_karty.RowHeadersWidth = 62;
-            dealer_karty.Size = new Size(441, 73);
-            dealer_karty.TabIndex = 7;
-            // 
-            // gracz_karty
-            // 
-            gracz_karty.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            gracz_karty.Location = new Point(189, 195);
-            gracz_karty.Name = "gracz_karty";
-            gracz_karty.RowHeadersWidth = 62;
-            gracz_karty.Size = new Size(441, 73);
-            gracz_karty.TabIndex = 8;
-            // 
             // suma_dealer
             // 
-            suma_dealer.Location = new Point(657, 88);
+            suma_dealer.Location = new Point(460, 173);
+            suma_dealer.Margin = new Padding(2);
             suma_dealer.Name = "suma_dealer";
-            suma_dealer.Size = new Size(66, 31);
+            suma_dealer.Size = new Size(47, 23);
             suma_dealer.TabIndex = 9;
             // 
             // suma_gracz
             // 
-            suma_gracz.Location = new Point(657, 210);
+            suma_gracz.Location = new Point(460, 558);
+            suma_gracz.Margin = new Padding(2);
             suma_gracz.Name = "suma_gracz";
-            suma_gracz.Size = new Size(66, 31);
+            suma_gracz.Size = new Size(47, 23);
             suma_gracz.TabIndex = 10;
             // 
             // suma_label
             // 
             suma_label.AutoSize = true;
-            suma_label.Location = new Point(657, 21);
+            suma_label.Location = new Point(460, 156);
+            suma_label.Margin = new Padding(2, 0, 2, 0);
             suma_label.Name = "suma_label";
-            suma_label.Size = new Size(57, 25);
+            suma_label.Size = new Size(37, 15);
             suma_label.TabIndex = 11;
             suma_label.Text = "Suma";
             // 
+            // reka_dealer
+            // 
+            reka_dealer.AutoScroll = true;
+            reka_dealer.Location = new Point(111, 31);
+            reka_dealer.Name = "reka_dealer";
+            reka_dealer.Size = new Size(321, 314);
+            reka_dealer.TabIndex = 12;
+            reka_dealer.Paint += flowLayoutPanel1_Paint;
+            // 
+            // reka_gracz
+            // 
+            reka_gracz.AutoScroll = true;
+            reka_gracz.Location = new Point(111, 405);
+            reka_gracz.Name = "reka_gracz";
+            reka_gracz.Size = new Size(321, 314);
+            reka_gracz.TabIndex = 13;
+            // 
+            // button_hit
+            // 
+            button_hit.Enabled = false;
+            button_hit.Location = new Point(130, 740);
+            button_hit.Name = "button_hit";
+            button_hit.Size = new Size(75, 23);
+            button_hit.TabIndex = 14;
+            button_hit.Text = "Dobierz";
+            button_hit.UseVisualStyleBackColor = true;
+            button_hit.Click += button_hit_Click;
+            // 
+            // button_hold
+            // 
+            button_hold.Enabled = false;
+            button_hold.Location = new Point(335, 740);
+            button_hold.Name = "button_hold";
+            button_hold.Size = new Size(75, 23);
+            button_hold.TabIndex = 15;
+            button_hold.Text = "Pas";
+            button_hold.UseVisualStyleBackColor = true;
+            button_hold.Click += button_hold_Click;
+            // 
+            // suma_label2
+            // 
+            suma_label2.AutoSize = true;
+            suma_label2.Location = new Point(460, 541);
+            suma_label2.Margin = new Padding(2, 0, 2, 0);
+            suma_label2.Name = "suma_label2";
+            suma_label2.Size = new Size(37, 15);
+            suma_label2.TabIndex = 16;
+            suma_label2.Text = "Suma";
+            // 
+            // log_box
+            // 
+            log_box.Location = new Point(599, 31);
+            log_box.Name = "log_box";
+            log_box.ReadOnly = true;
+            log_box.Size = new Size(365, 550);
+            log_box.TabIndex = 17;
+            log_box.Text = "";
+            // 
             // Blackjack
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(961, 569);
+            ClientSize = new Size(1010, 839);
+            Controls.Add(log_box);
+            Controls.Add(suma_label2);
+            Controls.Add(button_hold);
+            Controls.Add(button_hit);
+            Controls.Add(reka_gracz);
+            Controls.Add(reka_dealer);
             Controls.Add(suma_label);
             Controls.Add(suma_gracz);
             Controls.Add(suma_dealer);
-            Controls.Add(gracz_karty);
-            Controls.Add(dealer_karty);
             Controls.Add(saldo_ilosc);
             Controls.Add(saldo_label);
             Controls.Add(zaklad_numeric);
             Controls.Add(start_button);
             Controls.Add(gracz_label);
             Controls.Add(dealer_label);
+            Margin = new Padding(2);
             Name = "Blackjack";
             Text = "Blackjack";
             ((System.ComponentModel.ISupportInitialize)zaklad_numeric).EndInit();
-            ((System.ComponentModel.ISupportInitialize)dealer_karty).EndInit();
-            ((System.ComponentModel.ISupportInitialize)gracz_karty).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -170,10 +226,14 @@
         private NumericUpDown zaklad_numeric;
         private Label saldo_label;
         private Label saldo_ilosc;
-        private DataGridView dealer_karty;
-        private DataGridView gracz_karty;
         private TextBox suma_dealer;
         private TextBox suma_gracz;
         private Label suma_label;
+        private FlowLayoutPanel reka_dealer;
+        private FlowLayoutPanel reka_gracz;
+        private Button button_hit;
+        private Button button_hold;
+        private Label suma_label2;
+        private RichTextBox log_box;
     }
 }
--- WinFormsApp1/Blackjack.cs
+++ WinFormsApp1/Blackjack.cs
@@ -7,16 +7,380 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+using System.Threading;
 
 namespace WinFormsApp1
 {
     public partial class Blackjack : Form
     {
+
+        public enum Suit { hearts, diamonds, clubs, spades }
+        public enum Rank
+        {
+            Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten,
+            Jack, Queen, King, Ace
+        }
+
+        public class Card
+        {
+            public Suit Suit;
+            public Rank Rank;
+
+            public Card(Suit suit, Rank rank)
+            {
+                Suit = suit;
+                Rank = rank;
+            }
+
+            public override string ToString() => $"{Rank} of {Suit}";
+        }
+
+        public class Deck
+        {
+            private List<Card> _cards = new List<Card>();
+            private Random _rng = new Random();
+
+            public Deck()
+            {
+                Reset();
+            }
+
+            public void Reset()
+            {
+                _cards.Clear();
+                // Automatyczne generowanie 52 kart przy użyciu pętli po enumach
+                foreach (Suit s in Enum.GetValues(typeof(Suit)))
+                {
+                    foreach (Rank r in Enum.GetValues(typeof(Rank)))
+                    {
+                        _cards.Add(new Card(s, r));
+                    }
+                }
+            }
+
+            public void Shuffle()
+            {
+                // Algorytm Fisher-Yates - najpopularniejszy sposób tasowania
+                int n = _cards.Count;
+                while (n > 1)
+                {
+                    n--;
+                    int k = _rng.Next(n + 1);
+                    Card value = _cards[k];
+                    _cards[k] = _cards[n];
+                    _cards[n] = value;
+                }
+            }
+
+            public Card Draw()
+            {
+                if (_cards.Count == 0) throw new InvalidOperationException("Talia jest pusta!");
+
+                Card card = _cards[0];
+                _cards.RemoveAt(0);
+                return card;
+            }
+
+            public int cardscount()
+            {
+                int c = _cards.Count;
+                return c;
+            }
+        }
+
+        public int handsum(List<Card> hand)
+        {
+            int total = 0;
+            int aces = 0;
+            int idx = 0;
+            foreach (Card card in hand)
+            {
+                //poniewaz pierwsza karta dealera jest zakryta, musimy zastosować wyjątek
+                if(idx == 0 && hand == dealerhand && hand.Count == 2 && AlwaysShowFlag == false)
+                {
+                    idx++;
+                    continue;
+                }
+                if (card.Rank == Rank.Ace)
+                {
+                    aces++;
+                    total += 11;
+                }
+                else if (card.Rank >= Rank.Jack)
+                {
+                    total += 10;
+                }
+                else
+                {
+                    total += (int)card.Rank;
+                }
+                idx++;
+            }
+
+            //zmienność asa (11 lub 1)
+            if(hand == playerhand)
+            {
+                while (total > 21 && aces > 0)
+                {
+                    total = total - 10;
+                    aces--;
+                }
+                return total;
+            }
+            else
+            //soft 17 dla dealera
+            {
+                while ((total > 21 || total == 17) && aces > 0)
+                {
+                    total = total - 10;
+                    aces--;
+                }
+                return total;
+            }
+            
+        }
+
+
+        public void UpdateHand(List<Card> hand, FlowLayoutPanel panel)
+        {
+            panel.Controls.Clear();
+            string fileName;
+            string filePath;
+            int idx = 0;
+            foreach (Card card in hand)
+            {
+                PictureBox pb = new PictureBox();
+                pb.SizeMode = PictureBoxSizeMode.Zoom;
+                pb.Width = 100;
+                pb.Height = 145;
+                //poniewaz pierwsza karta dealera jest zakryta, musimy zastosować wyjątek
+                if (hand == dealerhand && hand.Count <= 2 && idx == 0 && AlwaysShowFlag == false)
+                {
+                    fileName = "back.png";
+                    filePath = Path.Combine(Application.StartupPath, "Karty", fileName);
+                }
+                else
+                {
+                    fileName = $"{card.Suit}_{(int)card.Rank}.png";
+                    filePath = Path.Combine(Application.StartupPath, "Karty", fileName);
+                }
+               
+                if (File.Exists(filePath))
+                {
+                    pb.Image = Image.FromFile(filePath);
+                }
+                else
+                {
+                    //czerwony kolor = błąd wczytywania obrazka.
+                    pb.BackColor = Color.Red;
+                }
+
+                panel.Controls.Add(pb);
+                idx++;
+            }
+        }
+
+
+        public void UpdateSum(List<Card> hand, TextBox textbox)
+        {
+            textbox.Clear();
+            int suma = 0;
+            //poniewaz pierwsza karta dealera jest zakryta, musimy zastosować wyjątek
+            if (hand == dealerhand && hand.Count == 2 && AlwaysShowFlag == false)
+            {
+                suma = handsum(hand);
+                textbox.Text = $">{suma.ToString()}";
+                return;
+            }
+            
+            suma = handsum(hand);
+            textbox.Text = suma.ToString();
+            return;
+        }
+
+
+
+
+
+
+
+        Deck deck = new Deck();
+        public List<Card> dealerhand = new List<Card>();
+        public List<Card> playerhand = new List<Card>();
+        public bool AlwaysShowFlag = false;
+
         private Form1 okno1;
         public Blackjack(Form1 form1)
         {
+
             InitializeComponent();
             this.okno1 = form1;
+            deck.Reset();
+            deck.Shuffle();
+            NowaGra();
+
+
+        }
+
+        private void Log(string message, Color color)
+        {
+            log_box.SelectionStart = log_box.TextLength;
+            log_box.SelectionLength = 0;
+
+            log_box.SelectionColor = color;
+            log_box.AppendText($"[{DateTime.Now:HH:mm:ss}] {message}{Environment.NewLine}");
+
+            // Automatyczne przewinięcie na dół
+            log_box.ScrollToCaret();
+        }
+
+        public async Task NowaGra()
+        {
+            Log("Rozpoczynam nową grę.", Color.Black);
+            AlwaysShowFlag = false;
+            //Jeśli talia się kończy, zresetuj talię
+            if (deck.cardscount() < 15)
+            {
+                deck.Reset();
+                deck.Shuffle();
+                Log("Talia się kończy, Dealer bierze nową talię i ją tasuje...", Color.Black);
+                
+            }
+            playerhand.Clear();
+            dealerhand.Clear();
+            reka_gracz.Controls.Clear();
+            reka_dealer.Controls.Clear();
+            await Task.Delay(1000);
+            //pierwsze rozdanie
+            playerhand.Add((Card)deck.Draw());
+            Log("Dobierasz kartę...", Color.Black);
+            UpdateHand(playerhand, reka_gracz);
+            UpdateSum(playerhand, suma_gracz);
+            await Task.Delay(500);
+
+            
+            dealerhand.Add((Card)deck.Draw());
+            Log("Dealer dobiera kartę...", Color.Black);
+            UpdateHand(dealerhand, reka_dealer);
+            await Task.Delay(500);
+
+            playerhand.Add((Card)deck.Draw());
+            Log("Dobierasz kartę...", Color.Black);
+            UpdateHand(playerhand, reka_gracz);
+            UpdateSum(playerhand, suma_gracz);
+            await Task.Delay(500);
+
+            dealerhand.Add((Card)deck.Draw());
+            Log("Dealer dobiera kartę...", Color.Black);
+            UpdateHand(dealerhand, reka_dealer);
+            UpdateSum(dealerhand, suma_dealer);
+            await Task.Delay(500);
+
+            
+            button_hold.Enabled = true;
+            button_hit.Enabled = true;
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private async void button_hold_Click(object sender, EventArgs e)
+        {
+            Log("Pasujesz. Tura dealera..", Color.Black);
+            AlwaysShowFlag = true;
+            button_hit.Enabled = false;
+            button_hold.Enabled = false;
+            await Task.Delay(1000);
+            Log("Dealer odkrywa karty...", Color.Black);
+            UpdateHand(dealerhand, reka_dealer);
+            UpdateSum(dealerhand, suma_dealer);
+            await Task.Delay(1000);
+            while (handsum(dealerhand) < 17)
+            {
+                Log("Suma kart na ręce dealera jest mniejsza od 17. Dealer dobiera kartę...", Color.Black);
+                await Task.Delay(500);
+                dealerhand.Add((Card)deck.Draw());
+                UpdateHand(dealerhand, reka_dealer);
+                UpdateSum(dealerhand, suma_dealer);
+
+            }
+            Log($"Suma kart na ręce dealera wynosi {handsum(dealerhand)}. Dealer kończy dobieranie.", Color.Black);
+            await Task.Delay(1000);
+
+            if (handsum(dealerhand) > 21)
+            {
+                Log("Suma kart na ręce dealera jest większa od 21, wygrywasz!", Color.Green);
+            }
+            else if (handsum(playerhand) > handsum(dealerhand))
+            {
+                Log("Posiadasz większą sumę kart od dealera, wygrywasz!", Color.Green);
+            }
+            else if (handsum(playerhand) < handsum(dealerhand))
+            {
+                Log("Dealer posiada większą sumę kart od ciebie, przegrywasz!", Color.Red);
+            }
+            else
+            {
+                Log("Posiadasz taką samą sumę kart co dealer, przegrywasz!", Color.Red);
+            }
+        }
+
+        private void dealer_label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button_hit_Click(object sender, EventArgs e)
+        {
+            Log("Dobierasz kartę...", Color.Black);
+            await Task.Delay(500);
+            playerhand.Add((Card)deck.Draw());
+            UpdateHand(playerhand, reka_gracz);
+            UpdateSum(playerhand, suma_gracz);
+            await Task.Delay(500);
+            if (handsum(playerhand) > 21)
+            {
+
+                Log("Suma kart jest większa od 21!", Color.Red);
+                AlwaysShowFlag = true;
+                button_hit.Enabled = false;
+                button_hold.Enabled = false;
+                await Task.Delay(1000);
+                Log("Dealer odkrywa karty...", Color.Black);
+                UpdateHand(dealerhand, reka_dealer);
+                UpdateSum(dealerhand, suma_dealer);
+                await Task.Delay(1000);
+                while (handsum(dealerhand) < 17)
+                {
+                    Log("Suma kart na ręce dealera jest mniejsza od 17. Dealer dobiera kartę...", Color.Black);
+                    await Task.Delay(500);
+                    dealerhand.Add((Card)deck.Draw());
+                    UpdateHand(dealerhand, reka_dealer);
+                    UpdateSum(dealerhand, suma_dealer);
+
+                }
+                Log($"Suma kart na ręce dealera wynosi {handsum(dealerhand)}. Dealer kończy dobieranie.", Color.Black);
+                await Task.Delay(1000);
+
+                if (handsum(dealerhand) > 21)
+                {
+                    Log("Suma kart na ręce dealera jest większa od 21, remis!", Color.Yellow);
+                }
+                else
+                {
+                    Log("Dealer posiada mniej niż 21, przegrana!", Color.Red);
+                }
+                
+            }
+        }
+
+        private void saldo_label_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
