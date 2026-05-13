c6afb318317c839fd844d899ef3e896f6343cfe3
nadia-olszewska <nadiagluszanowska@gmail.com>
2026-05-13 19:05:55 +0200

    Dodanie pilnowania odpowiednich kart z talii i przeciwnika - AI

--- WinFormsApp1/makao.Designer.cs
+++ WinFormsApp1/makao.Designer.cs
@@ -18,10 +18,11 @@
             this.lblTable = new System.Windows.Forms.Label();
             this.flowHand = new System.Windows.Forms.FlowLayoutPanel();
             this.btnDraw = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // lblTable (Karta na stole) <3
-            // 
+            this.btnMakao = new System.Windows.Forms.Button();
+            this.lblStats = new System.Windows.Forms.Label();
+            this.SuspendLayout(); 
+
+            // Karta na stole
             this.lblTable.BackColor = System.Drawing.Color.White;
             this.lblTable.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.lblTable.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold);
@@ -29,11 +30,9 @@
             this.lblTable.Name = "lblTable";
             this.lblTable.Size = new System.Drawing.Size(120, 160);
             this.lblTable.TabIndex = 0;
-            this.lblTable.Text = "??";
             this.lblTable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // flowHand (karty) <3
-            // 
+
+            // Panel kart gracza
             this.flowHand.AutoScroll = true;
             this.flowHand.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.flowHand.Dock = System.Windows.Forms.DockStyle.Bottom;
@@ -42,36 +41,61 @@
             this.flowHand.Padding = new System.Windows.Forms.Padding(10);
             this.flowHand.Size = new System.Drawing.Size(800, 150);
             this.flowHand.TabIndex = 1;
-            // 
-            // btnDraw (Przycisk dobierania) <3
-            // 
+            
+            // Przycisk Dobierz
             this.btnDraw.BackColor = System.Drawing.Color.Gold;
             this.btnDraw.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDraw.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
-            this.btnDraw.Location = new System.Drawing.Point(340, 220);
+            this.btnDraw.Location = new System.Drawing.Point(270, 220);
             this.btnDraw.Name = "btnDraw";
             this.btnDraw.Size = new System.Drawing.Size(120, 45);
             this.btnDraw.TabIndex = 2;
             this.btnDraw.Text = "DOBIERZ";
             this.btnDraw.UseVisualStyleBackColor = false;
             this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
-            // 
-            // makao <3
-            // 
+            
+            // Przycisk MAKAO
+            this.btnMakao.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnMakao.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMakao.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnMakao.Location = new System.Drawing.Point(410, 220);
+            this.btnMakao.Name = "btnMakao";
+            this.btnMakao.Size = new System.Drawing.Size(120, 45);
+            this.btnMakao.TabIndex = 3;
+            this.btnMakao.Text = "MAKAO!";
+            this.btnMakao.UseVisualStyleBackColor = false;
+            this.btnMakao.Click += new System.EventHandler(this.btnMakao_Click);
+            
+            // Licznik kart
+            this.lblStats.AutoSize = true;
+            this.lblStats.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblStats.ForeColor = System.Drawing.Color.White;
+            this.lblStats.Location = new System.Drawing.Point(12, 9);
+            this.lblStats.Name = "lblStats";
+            this.lblStats.Size = new System.Drawing.Size(200, 21);
+            this.lblStats.TabIndex = 4;
+            this.lblStats.Text = "Czekam na start...";
+            
+            // makao
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.ForestGreen;
             this.ClientSize = new System.Drawing.Size(800, 461);
+            this.Controls.Add(this.lblStats);
+            this.Controls.Add(this.btnMakao);
             this.Controls.Add(this.btnDraw);
             this.Controls.Add(this.flowHand);
             this.Controls.Add(this.lblTable);
             this.Name = "makao";
-            this.Text = "Makao Game";
+            this.Text = "Makao Game - Gracz vs Komputer";
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         private System.Windows.Forms.Label lblTable;
         private System.Windows.Forms.FlowLayoutPanel flowHand;
         private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Button btnMakao;
+        private System.Windows.Forms.Label lblStats;
     }
 }
--- WinFormsApp1/makao.cs
+++ WinFormsApp1/makao.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
+using System.Threading.Tasks;
 
 namespace WinFormsApp1
 {
     public partial class makao : Form
     {
+        private List<string> gameDeck = new List<string>();
+        private List<string> discardPile = new List<string>();
         private List<string> playerHand = new List<string>();
-        private string[] deck = {
-            "6♥", "7♥", "8♥", "9♥", "10♥", "J♥", "Q♥", "K♥", "A♥",
-            "6♠", "7♠", "8♠", "9♠", "10♠", "J♠", "Q♠", "K♠", "A♠",
-            "6♦", "7♦", "8♦", "9♦", "10♦", "J♦", "Q♦", "K♦", "A♦",
-            "6♣", "7♣", "8♣", "9♣", "10♣", "J♣", "Q♣", "K♣", "A♣"
-        };
+        private List<string> computerHand = new List<string>();
+
+        private bool playerSaidMakao = false;
+        private bool isPlayerTurn = true;
         private Random rand = new Random();
 
         public makao()
@@ -25,53 +26,190 @@ namespace WinFormsApp1
 
         private void StartNewGame()
         {
-            lblTable.Text = deck[rand.Next(deck.Length)];
-            if (lblTable.Text.Contains("♥") || lblTable.Text.Contains("♦")) lblTable.ForeColor = Color.Red;
+            InitializeFullDeck();
+            playerHand.Clear();
+            computerHand.Clear();
+            discardPile.Clear();
+            isPlayerTurn = true;
+            playerSaidMakao = false;
 
-            for (int i = 0; i < 5; i++) playerHand.Add(deck[rand.Next(deck.Length)]);
+            string startingCard = DrawFromDeckLogic();
+            lblTable.Text = startingCard;
+            UpdateTableColor(startingCard);
+
+            for (int i = 0; i < 5; i++)
+            {
+                playerHand.Add(DrawFromDeckLogic());
+                computerHand.Add(DrawFromDeckLogic());
+            }
+
+            UpdateStats();
             RefreshHand();
+        }
+
+        private void UpdateStats()
+        {
+            lblStats.Text = $"Twoje karty: {playerHand.Count} | Karty komputera: {computerHand.Count}";
+        }
+
+        private void InitializeFullDeck()
+        {
+            string[] suits = { "♥", "♠", "♦", "♣" };
+            string[] values = { "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+            gameDeck.Clear();
+            foreach (var suit in suits)
+                foreach (var val in values)
+                    gameDeck.Add(val + suit);
+        }
+
+        private string DrawFromDeckLogic()
+        {
+            if (gameDeck.Count == 0) ReshuffleDiscardPile();
+            if (gameDeck.Count == 0) return "6♥";
+
+            int index = rand.Next(gameDeck.Count);
+            string card = gameDeck[index];
+            gameDeck.RemoveAt(index);
+            return card;
+        }
+
+        private void ReshuffleDiscardPile()
+        {
+            string currentOnTable = lblTable.Text;
+            MessageBox.Show("🃏 Przetasowanie stosu wyrzuconych kart!", "Krupier");
+            gameDeck.AddRange(discardPile);
+            discardPile.Clear();
+            lblTable.Text = currentOnTable;
         }
 
         private void btnDraw_Click(object sender, EventArgs e)
         {
-            playerHand.Add(deck[rand.Next(deck.Length)]);
-            RefreshHand();
+            if (!isPlayerTurn) return;
+
+            playerHand.Add(DrawFromDeckLogic());
+            playerSaidMakao = false;
+            EndPlayerTurn();
         }
 
-        private void RefreshHand()
+        private void btnMakao_Click(object sender, EventArgs e)
         {
-            flowHand.Controls.Clear();
-            foreach (string card in playerHand)
+            if (playerHand.Count <= 2)
             {
-                Button cardBtn = new Button();
-                cardBtn.Text = card;
-                cardBtn.Size = new Size(75, 110);
-                cardBtn.BackColor = Color.White;
-                cardBtn.Font = new Font("Arial", 12, FontStyle.Bold);
-                cardBtn.FlatStyle = FlatStyle.Flat;
-
-                if (card.Contains("♥") || card.Contains("♦")) cardBtn.ForeColor = Color.Red;
-                else cardBtn.ForeColor = Color.Black;
-
-                cardBtn.Click += (s, e) => PlayCard(card);
-                flowHand.Controls.Add(cardBtn);
+                playerSaidMakao = true;
+                MessageBox.Show("Krzyknąłeś: MAKAO! Teraz wyrzuć kartę.", "Refleks");
+            }
+            else
+            {
+                MessageBox.Show("Masz za dużo kart na Makao!", "Zasady");
             }
         }
 
         private void PlayCard(string card)
         {
-            string tableCard = lblTable.Text;
-            bool sameValue = card.Substring(0, card.Length - 1) == tableCard.Substring(0, tableCard.Length - 1);
-            bool sameSuit = card.Last() == tableCard.Last();
+            if (!isPlayerTurn) return;
 
-            if (sameValue || sameSuit)
+            if (CanPlay(card, lblTable.Text))
             {
+                if (playerHand.Count == 2 && !playerSaidMakao)
+                {
+                    MessageBox.Show("ZAPOMNIAŁEŚ POWIEDZIEĆ MAKAO! Dobierasz 2 karty kary.", "Kara");
+                    playerHand.Add(DrawFromDeckLogic());
+                    playerHand.Add(DrawFromDeckLogic());
+                    playerSaidMakao = false;
+                    RefreshHand();
+                    UpdateStats();
+                    EndPlayerTurn();
+                    return;
+                }
+
+                discardPile.Add(lblTable.Text);
                 lblTable.Text = card;
-                lblTable.ForeColor = (card.Contains("♥") || card.Contains("♦")) ? Color.Red : Color.Black;
+                UpdateTableColor(card);
                 playerHand.Remove(card);
-                RefreshHand();
-                if (playerHand.Count == 0) MessageBox.Show("Wygrałeś!");
+
+                if (playerHand.Count == 0)
+                {
+                    MessageBox.Show("MAKAO I PO MAKALE! WYGRAŁEŚ! 🎉", "Zwycięstwo");
+                    StartNewGame();
+                    return;
+                }
+
+                EndPlayerTurn();
             }
+            else
+            {
+                MessageBox.Show("Ta karta nie pasuje!", "Błąd");
+            }
+        }
+
+        private void EndPlayerTurn()
+        {
+            isPlayerTurn = false;
+            playerSaidMakao = false;
+            UpdateStats();
+            RefreshHand();
+            ComputerTurn();
+        }
+
+        private async void ComputerTurn()
+        {
+            await Task.Delay(1500);
+
+            string tableCard = lblTable.Text;
+            string cardToPlay = computerHand.FirstOrDefault(c => CanPlay(c, tableCard));
+
+            if (cardToPlay != null)
+            {
+                if (computerHand.Count == 2)
+                {
+                    MessageBox.Show("Komputer krzyczy: MAKAO!", "AI");
+                }
+
+                discardPile.Add(lblTable.Text);
+                lblTable.Text = cardToPlay;
+                UpdateTableColor(cardToPlay);
+                computerHand.Remove(cardToPlay);
+            }
+            else
+            {
+                computerHand.Add(DrawFromDeckLogic());
+            }
+
+            if (computerHand.Count == 0)
+            {
+                MessageBox.Show("Komputer wygrał! Spróbuj ponownie.", "Porażka");
+                StartNewGame();
+                return;
+            }
+
+            isPlayerTurn = true;
+            UpdateStats();
+            RefreshHand();
+        }
+
+        private bool CanPlay(string card, string table)
+        {
+            string cV = card.Substring(0, card.Length - 1);
+            string tV = table.Substring(0, table.Length - 1);
+            return cV == tV || card.Last() == table.Last();
+        }
+
+        private void RefreshHand()
+        {
+            flowHand.Controls.Clear();
+            flowHand.Enabled = isPlayerTurn;
+            foreach (string card in playerHand)
+            {
+                Button cardBtn = new Button { Text = card, Size = new Size(75, 110), BackColor = Color.White, Font = new Font("Arial", 10, FontStyle.Bold), FlatStyle = FlatStyle.Flat };
+                if (card.Contains("♥") || card.Contains("♦")) cardBtn.ForeColor = Color.Red;
+                cardBtn.Click += (s, e) => PlayCard(card);
+                flowHand.Controls.Add(cardBtn);
+            }
+        }
+
+        private void UpdateTableColor(string card)
+        {
+            lblTable.ForeColor = (card.Contains("♥") || card.Contains("♦")) ? Color.Red : Color.Black;
         }
     }
 }
