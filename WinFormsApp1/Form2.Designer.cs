9e8ae2e7b2eff485b36456b2b05ead5dac3c72df
dzonekowy <dzonekowyh2o@gmail.com>
2026-05-13 13:19:36 +0200

    60% done

--- WinFormsApp1/Form2.Designer.cs
+++ WinFormsApp1/Form2.Designer.cs
@@ -41,14 +41,17 @@
             button8 = new Button();
             button9 = new Button();
             button10 = new Button();
+            label4 = new Label();
+            label5 = new Label();
             SuspendLayout();
             // 
             // button1
             // 
             button1.Font = new Font("Segoe UI", 20F);
-            button1.Location = new Point(608, 237);
+            button1.Location = new Point(695, 316);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(125, 51);
+            button1.Size = new Size(143, 68);
             button1.TabIndex = 5;
             button1.Text = "Start";
             button1.UseVisualStyleBackColor = true;
@@ -58,75 +61,87 @@
             // 
             button2.FlatAppearance.BorderSize = 0;
             button2.FlatStyle = FlatStyle.Popup;
-            button2.Location = new Point(983, 570);
+            button2.Location = new Point(1123, 760);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(70, 45);
+            button2.Size = new Size(80, 60);
             button2.TabIndex = 5;
             button2.UseVisualStyleBackColor = true;
             button2.Visible = false;
+            button2.Click += button2_Click;
             // 
             // button3
             // 
             button3.FlatAppearance.BorderSize = 0;
             button3.FlatStyle = FlatStyle.Popup;
-            button3.Location = new Point(895, 630);
+            button3.Location = new Point(1023, 840);
+            button3.Margin = new Padding(3, 4, 3, 4);
             button3.Name = "button3";
-            button3.Size = new Size(65, 45);
+            button3.Size = new Size(74, 60);
             button3.TabIndex = 5;
             button3.UseVisualStyleBackColor = true;
             button3.Visible = false;
+            button3.Click += button3_Click;
             // 
             // button4
             // 
             button4.FlatAppearance.BorderSize = 0;
             button4.FlatStyle = FlatStyle.Popup;
-            button4.Location = new Point(968, 620);
+            button4.Location = new Point(1106, 827);
+            button4.Margin = new Padding(3, 4, 3, 4);
             button4.Name = "button4";
-            button4.Size = new Size(70, 45);
+            button4.Size = new Size(80, 60);
             button4.TabIndex = 5;
             button4.UseVisualStyleBackColor = true;
             button4.Visible = false;
+            button4.Click += button4_Click;
             // 
             // button5
             // 
             button5.FlatAppearance.BorderSize = 0;
             button5.FlatStyle = FlatStyle.Popup;
-            button5.Location = new Point(819, 624);
+            button5.Location = new Point(936, 832);
+            button5.Margin = new Padding(3, 4, 3, 4);
             button5.Name = "button5";
-            button5.Size = new Size(70, 45);
+            button5.Size = new Size(80, 60);
             button5.TabIndex = 5;
             button5.UseVisualStyleBackColor = true;
             button5.Visible = false;
+            button5.Click += button5_Click;
             // 
             // button6
             // 
             button6.FlatAppearance.BorderSize = 0;
             button6.FlatStyle = FlatStyle.Popup;
-            button6.Location = new Point(809, 570);
+            button6.Location = new Point(925, 760);
+            button6.Margin = new Padding(3, 4, 3, 4);
             button6.Name = "button6";
-            button6.Size = new Size(70, 45);
+            button6.Size = new Size(80, 60);
             button6.TabIndex = 5;
             button6.UseVisualStyleBackColor = true;
             button6.Visible = false;
+            button6.Click += button6_Click;
             // 
             // button7
             // 
             button7.FlatAppearance.BorderSize = 0;
             button7.FlatStyle = FlatStyle.Popup;
-            button7.Location = new Point(892, 536);
+            button7.Location = new Point(1019, 715);
+            button7.Margin = new Padding(3, 4, 3, 4);
             button7.Name = "button7";
-            button7.Size = new Size(85, 50);
+            button7.Size = new Size(97, 67);
             button7.TabIndex = 5;
             button7.UseVisualStyleBackColor = true;
             button7.Visible = false;
+            button7.Click += button7_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Showcard Gothic", 39.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(492, 48);
+            label1.Location = new Point(562, 64);
             label1.Name = "label1";
-            label1.Size = new Size(369, 66);
+            label1.Size = new Size(465, 83);
             label1.TabIndex = 6;
             label1.Text = "FOUR NIGHTS";
             // 
@@ -134,20 +149,19 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Showcard Gothic", 25F);
-            label2.Location = new Point(608, 114);
+            label2.Location = new Point(695, 152);
             label2.Name = "label2";
-            label2.Size = new Size(116, 43);
+            label2.Size = new Size(139, 52);
             label2.TabIndex = 7;
             label2.Text = "WITH";
-            label2.Click += label2_Click;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Showcard Gothic", 39.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label3.Location = new Point(544, 157);
+            label3.Location = new Point(622, 209);
             label3.Name = "label3";
-            label3.Size = new Size(271, 66);
+            label3.Size = new Size(341, 83);
             label3.TabIndex = 8;
             label3.Text = "HYRAXES";
             // 
@@ -155,41 +169,71 @@
             // 
             button8.FlatAppearance.BorderSize = 0;
             button8.FlatStyle = FlatStyle.Popup;
-            button8.Location = new Point(414, 446);
+            button8.Location = new Point(473, 595);
+            button8.Margin = new Padding(3, 4, 3, 4);
             button8.Name = "button8";
-            button8.Size = new Size(115, 110);
+            button8.Size = new Size(131, 147);
             button8.TabIndex = 4;
             button8.UseVisualStyleBackColor = true;
             button8.Visible = false;
+            button8.Click += button8_Click;
             // 
             // button9
             // 
             button9.FlatAppearance.BorderSize = 0;
             button9.FlatStyle = FlatStyle.Popup;
-            button9.Location = new Point(584, 446);
+            button9.Location = new Point(667, 595);
+            button9.Margin = new Padding(3, 4, 3, 4);
             button9.Name = "button9";
-            button9.Size = new Size(115, 110);
+            button9.Size = new Size(131, 147);
             button9.TabIndex = 4;
             button9.UseVisualStyleBackColor = true;
             button9.Visible = false;
+            button9.Click += button9_Click;
             // 
             // button10
             // 
             button10.FlatAppearance.BorderSize = 0;
             button10.FlatStyle = FlatStyle.Popup;
-            button10.Location = new Point(747, 446);
+            button10.Location = new Point(854, 595);
+            button10.Margin = new Padding(3, 4, 3, 4);
             button10.Name = "button10";
-            button10.Size = new Size(115, 110);
+            button10.Size = new Size(131, 147);
             button10.TabIndex = 4;
             button10.UseVisualStyleBackColor = true;
             button10.Visible = false;
             button10.Click += button10_Click;
             // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Times New Roman", 32F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label4.Location = new Point(59, 40);
+            label4.Name = "label4";
+            label4.Size = new Size(179, 62);
+            label4.TabIndex = 9;
+            label4.Text = "12 PM";
+            label4.Visible = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Showcard Gothic", 39.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(572, 69);
+            label5.Name = "label5";
+            label5.Size = new Size(400, 83);
+            label5.TabIndex = 10;
+            label5.Text = "GAME OVER";
+            label5.Visible = false;
+            // 
             // FNAPP
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1264, 681);
+            ClientSize = new Size(1445, 908);
+            Controls.Add(label5);
+            Controls.Add(label4);
             Controls.Add(button10);
             Controls.Add(button9);
             Controls.Add(button8);
@@ -203,7 +247,7 @@
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
-            Margin = new Padding(2);
+            Margin = new Padding(2, 3, 2, 3);
             Name = "FNAPP";
             Text = "FNAPP";
             Load += FNAPP_Load;
@@ -225,5 +269,7 @@
         private Button button8;
         private Button button9;
         private Button button10;
+        private Label label4;
+        private Label label5;
     }
 }
--- WinFormsApp1/Form2.cs
+++ WinFormsApp1/Form2.cs
@@ -25,6 +25,7 @@ namespace WinFormsApp1
             label1.Left = (this.ClientSize.Width - label1.Width) / 2;
             label2.Left = (this.ClientSize.Width - label2.Width) / 2;
             label3.Left = (this.ClientSize.Width - label3.Width) / 2;
+            label5.Left = (this.ClientSize.Width - label5.Width) / 2;
 
         }
 
@@ -32,6 +33,8 @@ namespace WinFormsApp1
         {
 
         }
+
+        public string dir = Directory.GetCurrentDirectory().Replace("bin\\Debug\\net8.0-windows", "assets");
 
         private void FNAPP_Start(object sender, EventArgs e)
         {
@@ -49,9 +52,7 @@ namespace WinFormsApp1
             button8.Visible = true;
             button9.Visible = true;
             button10.Visible = true;
-
-            var dir = Directory.GetCurrentDirectory();
-            dir = dir.Replace("bin\\Debug\\net8.0-windows", "assets");
+            label4.Visible = true;
 
             button2.Image = Image.FromFile(dir + "\\cam3.png");
             button3.Image = Image.FromFile(dir + "\\cam-main.png");
@@ -63,54 +64,332 @@ namespace WinFormsApp1
             button9.Image = Image.FromFile(dir + "\\button.png");
             button10.Image = Image.FromFile(dir + "\\button.png");
 
-            print();
+            Print();
         }
 
         public bool isImage1Visible = true;
         public bool isImage2Visible = true;
+        public bool isImage2NewVisible = false;
         public bool isImage3Visible = true;
+        public bool isBattery100 = true;
+        public bool isBattery80 = false;
+        public bool isBattery60 = false;
+        public bool isBattery40 = false;
+        public bool isBattery20 = false;
+        public bool isBattery0 = false;
+        public bool isDoorLeft = false;
+        public bool isDoorRight = false;
 
-        private void print()
+        public bool isHyraxIn = false;
+        public bool isSzopIn = false;
+        public bool isKrokIn = false;
+        public bool isHyraxOver = false;
+        public bool isSzopOver = false;
+        public bool isKrokOver = false;
+
+        private async void Print()
         {
-            var dir = Directory.GetCurrentDirectory();
-            dir = dir.Replace("bin\\Debug\\net8.0-windows", "assets");
+            //
+            // worst possible code here
+            //
+
 
             Image image1 = Image.FromFile(dir + "\\main1.png");
             Image image2 = Image.FromFile(dir + "\\main2-dark.png");
-            Image image3 = Image.FromFile(dir + "\\main3.png");
+            Image window = Image.FromFile(dir + "\\main3.png");
+            Image image2new = Image.FromFile(dir + "\\main2.png");
+            Image doorLeft = Image.FromFile(dir + "\\door-left.png");
+            Image doorRight = Image.FromFile(dir + "\\door-right.png");
+            Image battery100 = Image.FromFile(dir + "\\battery100.png");
+            Image battery80 = Image.FromFile(dir + "\\battery80.png");
+            Image battery60 = Image.FromFile(dir + "\\battery60.png");
+            Image battery40 = Image.FromFile(dir + "\\battery40.png");
+            Image battery20 = Image.FromFile(dir + "\\battery20.png");
+            Image battery0 = Image.FromFile(dir + "\\battery0.png");
+
+            Image hyraxIn = Image.FromFile(dir + "\\hyrax-in.png");
+            Image shopIn = Image.FromFile(dir + "\\szop-in.png");
+            Image krokIn = Image.FromFile(dir + "\\krok-in.png");
+            Image krokInDark = Image.FromFile(dir + "\\krok-in-dark.png");
+            Image hyraxOver = Image.FromFile(dir + "\\hyrax1.png");
+            Image szopOver = Image.FromFile(dir + "\\szop1.png");
+            Image krokOver = Image.FromFile(dir + "\\krok1.png");
 
             Graphics g = this.CreateGraphics();
 
             if (isImage2Visible)
             {
                 g.DrawImage(image2, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+
+                if (isKrokIn)
+                {
+                    g.DrawImage(krokInDark, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+                }
             }
-            if(isImage1Visible)
+            if (isImage2NewVisible)
+            {
+                g.DrawImage(image2new, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+
+                if (isKrokIn)
+                {
+                    g.DrawImage(krokIn, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+                }
+            }
+            if (isImage1Visible)
             {
                 g.DrawImage(image1, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
             }
-            if(isImage3Visible)
+            if (isImage3Visible)
             {
-                g.DrawImage(image3, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+                g.DrawImage(window, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
             }
+            if(isHyraxIn)
+            {
+                g.DrawImage(hyraxIn, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+            }
+            if(isSzopIn)
+            {
+                g.DrawImage(shopIn, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+            }
+            if (isDoorLeft)
+            {
+                g.DrawImage(doorLeft, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+            }
+            if (isDoorRight)
+            {
+                g.DrawImage(doorRight, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+            }
+            if (isBattery100)
+            {
+                g.DrawImage(battery100, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+            }
+            if (isBattery80)
+            {
+                g.DrawImage(battery80, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+            }
+            if (isBattery60)
+            {
+                g.DrawImage(battery60, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+            }
+            if (isBattery40)
+            {
+                g.DrawImage(battery40, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+            }
+            if (isBattery20)
+            {
+                g.DrawImage(battery20, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+            }
+            if (isBattery0)
+            {
+                g.DrawImage(battery0, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+            }
+            if (isHyraxOver)
+            {
+                buttonsOff();
+                cameraOff();
+
+                g.DrawImage(image2, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+                g.DrawImage(image1, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+                g.DrawImage(window, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+
+
+                g.DrawImage(hyraxOver, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+
+                label5.Visible = true;
+            }
+            if (isSzopOver)
+            {
+                buttonsOff();
+                cameraOff();
+
+                g.DrawImage(image2, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+                g.DrawImage(image1, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+                g.DrawImage(window, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+
+                g.DrawImage(szopOver, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+
+                label5.Visible = true;
+            }
+            if (isKrokOver)
+            {
+                buttonsOff();
+                cameraOff();
+
+                g.DrawImage(image2, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+                g.DrawImage(image1, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+                g.DrawImage(window, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+
+                g.DrawImage(krokOver, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+
+                label5.Visible = true;
+            }
+
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        public void buttonsOff()
         {
-
+            button8.Visible = false;
+            button9.Visible = false;
+            button10.Visible = false;
         }
+
+        public void cameraOff()
+        {
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = false;
+        }
+
+
+
 
         private void button10_Click(object sender, EventArgs e)
         {
-            var dir = Directory.GetCurrentDirectory();
-            dir = dir.Replace("bin\\Debug\\net8.0-windows", "assets");
-            Image image1 = Image.FromFile(dir + "\\door-right.png");
+            if (!isDoorRight)
+            {
+                Image image1 = Image.FromFile(dir + "\\door-right.png");
 
-            button10.Image = Image.FromFile(dir + "\\button-active.png");
+                button10.Image = Image.FromFile(dir + "\\button-active.png");
+
+                Graphics g = this.CreateGraphics();
+
+                g.DrawImage(image1, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+
+                isDoorRight = true;
+            }
+            else
+            {
+                button10.Image = Image.FromFile(dir + "\\button.png");
+                isDoorRight = false;
+                Print();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (!isImage2NewVisible)
+            {
+                button9.Image = Image.FromFile(dir + "\\button-active.png");
+
+                isImage2NewVisible = true;
+                isImage2Visible = false;
+                Print();
+            }
+            else
+            {
+                button9.Image = Image.FromFile(dir + "\\button.png");
+                isImage2NewVisible = false;
+                isImage2Visible = true;
+                Print();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (!isDoorLeft)
+            {
+                Image image1 = Image.FromFile(dir + "\\door-left.png");
+
+                button8.Image = Image.FromFile(dir + "\\button-active.png");
+
+                Graphics g = this.CreateGraphics();
+
+                g.DrawImage(image1, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+
+                isDoorLeft = true;
+            }
+            else
+            {
+                button8.Image = Image.FromFile(dir + "\\button.png");
+                isDoorLeft = false;
+                Print();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            buttonsOff();
+
+            Image image1 = Image.FromFile(dir + "\\cam1-main.png");
+            Image image2 = Image.FromFile(dir + "\\cam-blur.png");
+
 
             Graphics g = this.CreateGraphics();
 
             g.DrawImage(image1, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+            g.DrawImage(image2, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            buttonsOff();
+
+            Image image1 = Image.FromFile(dir + "\\cam2-main.png");
+            Image image2 = Image.FromFile(dir + "\\cam-blur.png");
+
+
+            Graphics g = this.CreateGraphics();
+
+            g.DrawImage(image1, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+            g.DrawImage(image2, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public bool isHyraxInCam5 = true;
+        public bool isSzopInCam5 = true;
+        public bool isKrokInCam5 = true;
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            buttonsOff();
+
+            Image image1 = Image.FromFile(dir + "\\cam5-main.png");
+            Image image2 = Image.FromFile(dir + "\\cam-blur.png");
+            Image hyraxIn = Image.FromFile(dir + "\\cam5-hyrax.png");
+            Image szopIn = Image.FromFile(dir + "\\cam5-szop.png");
+            Image krokIn = Image.FromFile(dir + "\\cam5-krok.png");
+
+            Graphics g = this.CreateGraphics();
+
+            g.DrawImage(image1, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+
+            if(isHyraxInCam5)
+            {
+                g.DrawImage(hyraxIn, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+            }
+            if(isSzopInCam5)
+            {
+                g.DrawImage(szopIn, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+            }
+            if(isKrokInCam5)
+            {
+                g.DrawImage(krokIn, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+            }
+
+            g.DrawImage(image2, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            button8.Visible = true;
+            button9.Visible = true;
+            button10.Visible = true;
+
+            Print();
         }
     }
 }
