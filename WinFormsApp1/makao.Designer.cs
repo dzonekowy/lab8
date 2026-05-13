0c14c3e17a5f2f2b89551b3fc8c0706fc45a6d8d
nadia-olszewska <nadiagluszanowska@gmail.com>
2026-05-13 18:30:01 +0200

    Pierwsza wersja makao - 1os

---
+++ WinFormsApp1/makao.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WinFormsApp1
+{
+    partial class makao
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblTable = new System.Windows.Forms.Label();
+            this.flowHand = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTable (Karta na stole) <3
+            // 
+            this.lblTable.BackColor = System.Drawing.Color.White;
+            this.lblTable.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTable.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold);
+            this.lblTable.Location = new System.Drawing.Point(340, 40);
+            this.lblTable.Name = "lblTable";
+            this.lblTable.Size = new System.Drawing.Size(120, 160);
+            this.lblTable.TabIndex = 0;
+            this.lblTable.Text = "??";
+            this.lblTable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // flowHand (karty) <3
+            // 
+            this.flowHand.AutoScroll = true;
+            this.flowHand.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.flowHand.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowHand.Location = new System.Drawing.Point(0, 311);
+            this.flowHand.Name = "flowHand";
+            this.flowHand.Padding = new System.Windows.Forms.Padding(10);
+            this.flowHand.Size = new System.Drawing.Size(800, 150);
+            this.flowHand.TabIndex = 1;
+            // 
+            // btnDraw (Przycisk dobierania) <3
+            // 
+            this.btnDraw.BackColor = System.Drawing.Color.Gold;
+            this.btnDraw.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDraw.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnDraw.Location = new System.Drawing.Point(340, 220);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(120, 45);
+            this.btnDraw.TabIndex = 2;
+            this.btnDraw.Text = "DOBIERZ";
+            this.btnDraw.UseVisualStyleBackColor = false;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // makao <3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(800, 461);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.flowHand);
+            this.Controls.Add(this.lblTable);
+            this.Name = "makao";
+            this.Text = "Makao Game";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Label lblTable;
+        private System.Windows.Forms.FlowLayoutPanel flowHand;
+        private System.Windows.Forms.Button btnDraw;
+    }
+}
---
+++ WinFormsApp1/makao.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class makao : Form
+    {
+        private List<string> playerHand = new List<string>();
+        private string[] deck = {
+            "6♥", "7♥", "8♥", "9♥", "10♥", "J♥", "Q♥", "K♥", "A♥",
+            "6♠", "7♠", "8♠", "9♠", "10♠", "J♠", "Q♠", "K♠", "A♠",
+            "6♦", "7♦", "8♦", "9♦", "10♦", "J♦", "Q♦", "K♦", "A♦",
+            "6♣", "7♣", "8♣", "9♣", "10♣", "J♣", "Q♣", "K♣", "A♣"
+        };
+        private Random rand = new Random();
+
+        public makao()
+        {
+            InitializeComponent();
+            StartNewGame();
+        }
+
+        private void StartNewGame()
+        {
+            lblTable.Text = deck[rand.Next(deck.Length)];
+            if (lblTable.Text.Contains("♥") || lblTable.Text.Contains("♦")) lblTable.ForeColor = Color.Red;
+
+            for (int i = 0; i < 5; i++) playerHand.Add(deck[rand.Next(deck.Length)]);
+            RefreshHand();
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            playerHand.Add(deck[rand.Next(deck.Length)]);
+            RefreshHand();
+        }
+
+        private void RefreshHand()
+        {
+            flowHand.Controls.Clear();
+            foreach (string card in playerHand)
+            {
+                Button cardBtn = new Button();
+                cardBtn.Text = card;
+                cardBtn.Size = new Size(75, 110);
+                cardBtn.BackColor = Color.White;
+                cardBtn.Font = new Font("Arial", 12, FontStyle.Bold);
+                cardBtn.FlatStyle = FlatStyle.Flat;
+
+                if (card.Contains("♥") || card.Contains("♦")) cardBtn.ForeColor = Color.Red;
+                else cardBtn.ForeColor = Color.Black;
+
+                cardBtn.Click += (s, e) => PlayCard(card);
+                flowHand.Controls.Add(cardBtn);
+            }
+        }
+
+        private void PlayCard(string card)
+        {
+            string tableCard = lblTable.Text;
+            bool sameValue = card.Substring(0, card.Length - 1) == tableCard.Substring(0, tableCard.Length - 1);
+            bool sameSuit = card.Last() == tableCard.Last();
+
+            if (sameValue || sameSuit)
+            {
+                lblTable.Text = card;
+                lblTable.ForeColor = (card.Contains("♥") || card.Contains("♦")) ? Color.Red : Color.Black;
+                playerHand.Remove(card);
+                RefreshHand();
+                if (playerHand.Count == 0) MessageBox.Show("Wygrałeś!");
+            }
+        }
+    }
+}
